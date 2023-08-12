e7acf4cf3d28c3fe361596402827e8e135b3b80d
pomzy001 <dpr4science@gmail.com>
2023-08-11 18:54:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdib.h>
+#include <string.h>
+/**
+ * main - Print the Alphabets without q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (int i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n);
+	return (0);
+}

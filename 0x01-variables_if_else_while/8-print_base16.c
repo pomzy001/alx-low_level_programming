1ea1f5971b8523d0ed36a3173295f1a6ea16538f
pomzy001 <dpr4science@gmail.com>
2023-08-11 20:45:28 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print numbers between 0 and 9
+ * and alphabets a to f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}

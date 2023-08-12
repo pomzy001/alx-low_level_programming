6173cc895547a953b57bc93e9f10e472338a6988
pomzy001 <dpr4science@gmail.com>
2023-08-11 20:22:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - prints alphabets in
+ * in reverse ordee
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alp[i] = "abcdefghijklmnopqrztuvwxyz";
+	int i;
+
+	for (i = 25; i <= 0; i --)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}

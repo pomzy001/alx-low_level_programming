c99df236a67eaa6046c8829d04ac2aea1c516030
pomzy001 <dpr4science@gmail.com>
2023-08-11 18:10:05 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar("\n");
+	return (0);
+}

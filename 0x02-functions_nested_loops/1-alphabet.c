25ecc83f9be953aeabb279f0ef30da17b1188b8c
pomzy001 <dpr4science@gmail.com>
2023-08-16 09:01:08 -0700

    Update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* print_alphabet - print all alphabet in lowercase
+*/
+void print_alphabet(void)
+	{
+	char az;
+for (az = 'a'; az <= 'z'; az++)
+	_putchar(az);
+	_putchar('\n');
+	}

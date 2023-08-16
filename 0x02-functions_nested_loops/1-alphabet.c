4b0b3b37ee20a6082efc2f759307c02ecf660613
pomzy001 <dpr4science@gmail.com>
2023-08-16 13:09:49 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,8 +4,9 @@
 */
 void print_alphabet(void)
 	{
-		char letter;
-			for (letter = 'a'; letter <= 'z'; letter++)
-	_putchar(letter);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 	_putchar('\n');
 	}

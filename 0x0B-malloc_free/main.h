7e5a1bf85dc355b3a7680d091d0e1b0b8d3742d3
pomzy001 <dpr4science@gmail.com>
2023-09-05 19:49:49 -0700

    updated

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -8,5 +8,6 @@ void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
 int _putchar(char c);
+char *create_array(unsigned int size, char c);
 
 #endif

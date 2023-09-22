acee9b0696361bc66c65abcad66df641d6fac54d
pomzy001 <dpr4science@gmail.com>
2023-09-22 13:58:31 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,22 +16,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	unsigned int len = 0;
 
 	while (str[len])
-		len++
+		len++;
+
 	new = malloc(sizeof(list_t);
-			if (!new)
+		if (!new)
 			return (NULL);
 
-			new->str = strdup(str);
-			new->len = len;
-			new->next = NULL;
+		new->str = strdup(str);
+		new->len = len;
+		new->next = NULL;
 
-			if (*head == NULL)
-			{
+		if (*head == NULL)
+		{
 			*head = new;
 			return (new);
-			}
+		}
+
 	while (temp->next)
-	temp = temp->next;
+		temp = temp->next;
 
 	temp->next = new;
 	return (new);

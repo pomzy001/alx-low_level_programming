4dfac607c46e000d82f2fa2f4f360b44a0517828
pomzy001 <dpr4science@gmail.com>
2023-09-28 03:41:51 -0700

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -25,7 +25,54 @@ size_t looped_listint_len(const listint_t *head)
 		if (tortoise = hare)
 		{
 			tortoise = head
-				while (tortoise != hare)
-				{
-					nodes++
-						tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++
+				tortoise = tortoise->next;
+			}
+			return (nodes);
+		}
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+
+/**
+ * print_listint_safe - prints a list of listint safely
+ * @head: A pointer to the head of the listint_t list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, index = 0;
+
+	nodes = looped_listint_len(head);
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("->[%p] %d\n", (void *)head, head->n);
+	}
+	return (nodes);
+}
+

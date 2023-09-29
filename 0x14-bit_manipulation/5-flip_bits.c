877cb88114392de923ced18017e5646bc97e602c
pomzy001 <dpr4science@gmail.com>
2023-09-29 13:43:54 -0700

    update

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -17,7 +17,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	for (i = 63; i >= 0; i--)
 	{
 		current = exclusive >> i;
-		if (count & 1)
+		if (current & 1)
 			count++;
 	}
 	return (count);

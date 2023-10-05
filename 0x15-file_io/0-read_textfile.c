e8c88e59015f229d0adb8b371510277100a44f8d
pomzy001 <dpr4science@gmail.com>
2023-10-05 11:45:21 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,46 +12,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	/**check for NULL filename*/
-	if (filename == NULL)
+	char *buffer;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
 		return (0);
+	buffer = malloc(sizeof(char)* letters);
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
 
-	/**open the file for reading*/
-	FILE *file = fopen(filename, "r");
-
-	if (file == NULL)
-		return (0);
-
-	/**Allocate memory for a buffer to read the file content*/
-	char *buffer = (char *)malloc(letters + 1);
-
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return (0);
-	}
-
-	/**Read from the file into buffer*/
-	size_t bytesRead = fread(buffer, 1, letters, file);
-
-	if (bytesRead <= 0)
-	{
-		free(buffer);
-		fclose(file);
-		return (0);
-	}
-	/**Null terminating the string*/
-	buffer[bytesRead] = '\0';
-	/**print to stdout*/
-	ssize_t bytesWritten = fwrite(buffer, 1, bytesRead, stdout);
-
-	fclose(file);
 	free(buffer);
-	if (bytesWritten != bytesRead)
-	{
-		return (0);
-	}
-	return (bytesWritten);
+	close(fd);
+	return (w);
 }
 
 
